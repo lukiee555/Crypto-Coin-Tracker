7beda56e87be8f35bb225e4bf6a2618280de8fe1
lukiee555 <sonilokesh24@gmail.com>
2018-03-17 01:57:31 +0530

    Complete

--- app/src/main/java/com/crypto/cryptocointracker/Adapter/CoinAdapter.kt
+++ app/src/main/java/com/crypto/cryptocointracker/Adapter/CoinAdapter.kt
@@ -1,10 +1,17 @@
 package com.crypto.cryptocointracker.Adapter
 
 import android.app.Activity
+import android.graphics.Color
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.crypto.cryptocointracker.Common.Common
+import com.crypto.cryptocointracker.Interface.ILoadMore
 import com.crypto.cryptocointracker.Model.CoinModel
+import com.crypto.cryptocointracker.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.coin_layout.view.*
 
 /**
@@ -23,20 +30,87 @@ class CoinViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 }
 
 
-class coinAdapter(recyclerView: RecyclerView, activity: Activity,var items:List<CoinModel>): RecyclerView.Adapter<CoinViewHolder>() {
+class CoinAdapter(recyclerView: RecyclerView, internal var activity: Activity,var items:List<CoinModel>): RecyclerView.Adapter<CoinViewHolder>() {
 
-    
+    internal var loadMore:ILoadMore?=null
+    var isLoadng:Boolean=false
+    var visiableThreshold=5
+    var lastVisiableItem:Int=0
+    var totalItemCount:Int=0
+    init {
+        val linearLayout = recyclerView.layoutManager as LinearLayoutManager
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                totalItemCount = linearLayout.itemCount
+                lastVisiableItem = linearLayout.findLastVisibleItemPosition()
+                if(!isLoadng && totalItemCount <= lastVisiableItem+visiableThreshold){
+                    if(loadMore != null){
+                        loadMore!!.onLoadMore()
+                    }
+                    isLoadng = true
+                }
+            }
+        })
+    }
+
+    fun setLoadMore(loadMore:ILoadMore){
+        this.loadMore = loadMore
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CoinViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(activity)
+                .inflate(R.layout.coin_layout,parent,false)
+        return CoinViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return  items.size
     }
 
     override fun onBindViewHolder(holder: CoinViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val coinModel = items.get(position)
+
+        val item = holder as CoinViewHolder
+
+        item.coinName.text = coinModel.name
+        item.coinSymbol.text = coinModel.symbol
+        item.coinPrice.text = coinModel.price_usd
+        item.onHourChange.text = coinModel.percent_change_1h+"%"
+        item.twentyFourChange.text = coinModel.percent_change_24h+"%"
+        item.sevenDaysChange.text = coinModel.percent_change_7d+"%"
+
+        Picasso.with(activity.baseContext)
+                .load(StringBuilder(Common.imageUrl).append(coinModel.symbol!!.toLowerCase())
+                        .append(".png")
+                        .toString()).into(item.coinIcon)
+
+        //set color
+        item.onHourChange.setTextColor(if (coinModel.percent_change_1h!!.contains("-"))
+                                            Color.parseColor("#FF0000")
+                                      else
+                                            Color.parseColor("#32cd32"))
+        item.twentyFourChange.setTextColor(if (coinModel.percent_change_24h!!.contains("-"))
+            Color.parseColor("#FF0000")
+        else
+            Color.parseColor("#32cd32"))
+
+        item.sevenDaysChange.setTextColor(if (coinModel.percent_change_7d!!.contains("-"))
+            Color.parseColor("#FF0000")
+        else
+            Color.parseColor("#32cd32"))
+
+    }
+
+    fun setLoaded(){
+        isLoadng = false
+    }
+
+    fun updateData(coinModels:List<CoinModel>){
+        this.items = coinModels
+        notifyDataSetChanged()
+
     }
 
 }
--- app/src/main/java/com/crypto/cryptocointracker/MainActivity.kt
+++ app/src/main/java/com/crypto/cryptocointracker/MainActivity.kt
@@ -2,13 +2,112 @@ package com.crypto.cryptocointracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.widget.Toast
+import com.crypto.cryptocointracker.Adapter.CoinAdapter
+import com.crypto.cryptocointracker.Common.Common
+import com.crypto.cryptocointracker.Interface.ILoadMore
+import com.crypto.cryptocointracker.Model.CoinModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ILoadMore {
+    //Declare variables
+
+    internal var items:MutableList<CoinModel> = ArrayList()
+    internal  lateinit var adapter: CoinAdapter
+    internal lateinit var client: OkHttpClient
+    internal lateinit var request:Request
+
+    override fun onLoadMore() {
+            if(items.size <= Common.MAX_COIN_LOAD)
+                loadNext10Coin(items.size)
+            else
+                Toast.makeText(this@MainActivity,"Data max is "+ Common.MAX_COIN_LOAD,Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
+    private fun loadNext10Coin(index: Int) {
+        client = OkHttpClient()
+        request = Request.Builder()
+                .url(String.format("https://api.coinmarketcap.com/v1/ticker/?limit=10",index))
+                .build()
+
+        swipe_to_refresh.isRefreshing=true
+        client.newCall(request)
+                .enqueue(object :Callback{
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        Log.d("ERROR",e.toString())
+                    }
+
+                    override fun onResponse(call: Call?, response: Response) {
+                        val body = response.body()!!.string()
+                        val gson = Gson()
+                        val newItems = gson.fromJson<List<CoinModel>>(body,object : TypeToken<List<CoinModel>>(){}.type)
+                        runOnUiThread {
+                            items.addAll(newItems)
+                            adapter.setLoaded()
+                            adapter.updateData(items)
+
+                            swipe_to_refresh.isRefreshing=false
+                        }
+
+                    }
+
+                })
+    }
+
+    private fun loadFirst10Coin() {
+        client = OkHttpClient()
+        request = Request.Builder()
+                .url(String.format("https://api.coinmarketcap.com/v1/ticker/?limit=10"))
+                .build()
+
+        client.newCall(request)
+                .enqueue(object :Callback{
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        Log.d("ERROR",e.toString())
+                    }
+
+                    override fun onResponse(call: Call?, response: Response) {
+                        val body = response.body()!!.string()
+                        val gson = Gson()
+                        items = gson.fromJson(body,object : TypeToken<List<CoinModel>>(){}.type)
+                        runOnUiThread {
+                            adapter.updateData(items)
+
+                        }
+
+                    }
+
+                })
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        swipe_to_refresh.post { loadFirst10Coin() }
 
+        swipe_to_refresh.setOnRefreshListener {
+            items.clear()
+            loadFirst10Coin()
+            setUpAdapter()
+        }
+        coin_recycler_view.layoutManager = LinearLayoutManager(this)
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter() {
+        adapter = CoinAdapter(coin_recycler_view,this@MainActivity,items)
+        coin_recycler_view.adapter = adapter
+        adapter.setLoadMore(this)
     }
 }
